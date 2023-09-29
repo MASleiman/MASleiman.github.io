ae1681fd619c6fbef4437ad0cd6067ba6a13d896
Mohamad Alee Sleiman <masleima@uwaterloo.ca>
2023-09-28 20:50:20 -0400

    deploy

---
+++ src/data/constants.js
@@ -0,0 +1,387 @@
+export const Bio= {
+    name: "Mohamad Alee Sleiman",
+    roles: [
+      "Mechanical Engineer",
+      "Creator",
+      "Robotics Enthusiast",
+      "Programmer"
+    ],
+    description:
+      "I am a dedicated Mechanical Engineer with a strong foundation in Quality and Manufacturing. Proficient in CAD and FEA, I thrive on solving complex engineering problems. With a relentless pursuit of innovation and a commitment to sustainable design, I am poised to make a lasting impact in the world of engineering.",
+    github: "https://github.com/MSleim",
+    
+    //Update these
+    resume:
+      "https://drive.google.com/file/d/13FNleS5qnn3Kj3EW2lH6Kfjub_GW7Yns/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/ma-sleiman/",
+  };
+  
+  export const skills = [
+    {
+      title: "CAD Software",
+      skills: [
+        {
+          name: "SolidWorks",
+          image:
+            "https://www.processia.com/wp-content/uploads/2022/03/3ds_brand_icons_cmyk_solidworks-250x250-1.png",
+        },
+        {
+          name: "Siemens NX",
+          image:
+            "https://upload.wikimedia.org/wikipedia/en/5/51/Siemens_NX_Logo.png",
+        },
+        {
+          name: "AutoCAD",
+          image:
+            "https://seeklogo.com/images/A/autocad-logo-69326D7728-seeklogo.com.png",
+        },
+        {
+          name: "Altair HyperWorks",
+          image:
+            "https://static.wixstatic.com/media/6e459c_4c343f3437f54c1b992e86abda706601~mv2.png/v1/fill/w_699,h_699,al_c,q_90/6e459c_4c343f3437f54c1b992e86abda706601~mv2.png",
+        },
+        {
+          name: "COMSOL",
+          image:
+            "https://audioproducteducationinstitute.org/wp-content/uploads/2022/01/Comsol_logo-blueMask-1024x306.png",
+        },
+      ],
+    },
+    {
+      title: "Design Process",
+      skills: [
+        {
+          name: "GD&T",
+          image: "https://bwl-gdandtbasics.imgix.net/2014/12/Position-symbol-svg.svg?auto=format",
+        },
+        {
+          name: "FEA",
+          image:
+            "https://www.formulastudent.de/typo3temp/_processed_/4/b/csm_Front_Upright_472x274_a1c1de6ca3.png",
+        },
+        {
+          name: "Quality Assurance",
+          image: "https://banner2.cleanpng.com/20180510/paq/kisspng-quality-assurance-qa-apprenticeships-slough-train-5af45554a13494.3965473615259620686603.jpg",
+        },
+        {
+          name: "Machine Shop Fabrication",
+          image:
+            "https://cdn.pixabay.com/photo/2012/04/12/13/58/handsaw-30137_1280.png",
+        },
+      ],
+    },
+    {
+      title: "Languages",
+      skills: [
+        {
+          name: "Java",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+        },
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "C/C++",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1822px-ISO_C%2B%2B_Logo.svg.png",
+        },
+        {
+          name: "MATLAB",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/2/21/Matlab_Logo.png",
+        },
+        {
+          name: "RobotC",
+          image:
+            "https://botbench.com/blog/wp-content/uploads/2011/08/logo.png",
+        },
+        {
+          name: "LaTeX",
+          image:
+            "https://i.stack.imgur.com/Sx2ec.png",
+        },
+        // {
+        //   name: "Pygame",
+        //   image:
+        //     "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+        // },
+        // {
+        //   name: "NumPy",
+        //   image:
+        //     "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+        // },
+        // {
+        //   name: "Matplotlib",
+        //   image:
+        //     "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+        // },
+        // {
+        //   name: "OpenCV",
+        //   image:
+        //     "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+        // },
+        // {
+        //   name: "Dlib",
+        //   image:
+        //     "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+        // },
+        {
+          name: "React JS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
+        },
+      ],
+    },
+    {
+      title: "Software",
+      skills: [
+        {
+          name: "Git",
+          image:
+            "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "Visual Studio",
+          image:
+            "https://1000logos.net/wp-content/uploads/2023/04/Visual-Studio-logo.png",
+        },
+        {
+          name: "Jupyter",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Jupyter_logo.svg/883px-Jupyter_logo.svg.png",
+        },
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "https://images.crunchbase.com/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/hpax2iqf1wxrodpj03j2",
+      role: "Manufacturing Engineer",
+      company: "Magna: Modatek Systems",
+      date: "Sept 2023 - Present",
+      desc: "In progress. \n Expected duties: Utilizing CAD software to create product overlays and 2D drawings, as well as generating stamping fast blanks and summarizing the stamping process. Additionally, responsibilities encompass coordinating and managing product data and bills of materials (BOM), designing and arranging meetings and presentations, and maintaining organized project-related customer and internal information.",
+      skills: [
+        "SolidWorks",
+        "Time Studies",
+        "MS Excel",
+        "Design",
+        "BOM Manipulation",
+      ]
+    },
+    {
+      id: 0,
+      img: "https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_fill,f_auto,h_200,w_200/v1403818360/npo3wfmpth8pjggtvfsn.png",
+      role: "SDI and CSW Core Member",
+      company: "UWAFT EcoCar Design Team",
+      date: "May 2023 - Present",
+      desc: "Creating a Python-based driver modeling system for detecting drowsiness, improved CAD design organization with new naming conventions, designing mounts in 3D CAD. Ultimatley, working towards the optimization of the electric powertrain and autonomy for the 2023 Cadillac Lyriq vehicle.",
+      skills: [
+        "Python",
+        "Dlib",
+        "Matplotlib",
+        "OpenCV",
+        "Siemens NX",
+        "Altair HyperMesh",
+        "Altair HyperWorks",
+      ]
+    },
+    {
+      id: 1,
+      img: "https://images.crunchbase.com/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/v1481894074/uhrzfokpawwomr88tbra.png",
+      role: "Quality Assurance",
+      company: "SkyJack Inc",
+      date: "Jan 2023 - Apr 2023",
+      desc: "Reduced defects by 6% through operator training, monitored quality efficiently with Excel, ManuX, and SolidWorks PDM, and improved data retrieval by 20% with master trackers. Contributed to successful PPAP reports, warranty testing, and inspections, and implemented 5S for enhanced operational efficiency and software skill development.",
+      skills: [
+        "Excel",
+        "SolidWorks PDM",
+        "Gauge R&R",
+        "Root-Cause Analysis",
+        "GD&T",
+        "ManuX",
+        "Data Analysis",
+      ],
+    },
+    // {
+    //   id: 2,
+    //   img: "https://media.licdn.com/dms/image/C510BAQGSObrO0QPlMQ/company-logo_200_200/0/1555704980721?e=1692835200&v=beta&t=FjOWcUVVNzqFbTmmp0iXQkv5keBQC5lxIQ7K6rmMSMQ",
+    //   role: "Open Source Contributor ",
+    //   company: "GirlScript Summer of Code",
+    //   date: "May 2023 - Present",
+    //   desc: "Contributing to different open-source projects and learn from industry experts",
+    //   doc: "https://media.licdn.com/dms/image/D4D22AQGzSpgFaR4hsQ/feedshare-shrink_800/0/1684079632872?e=1687996800&v=beta&t=i0xWn_k32Oj9SxKfc6rARSbOghXD9Ge6WyEb2p2vvrc",
+    // },
+    // {
+    //   id: 3,
+    //   img: "https://media.licdn.com/dms/image/D4D0BAQGfGQHEJt8gPA/company-logo_100_100/0/1666002815185?e=1692230400&v=beta&t=c4obZda4Uh9q6vOcmCbxjoSiExvbvYwEHUlgdY0tU-4",
+    //   role: "Android Developer",
+    //   company: "DSC KIIT",
+    //   date: "Nov2021 - Present",
+    //   desc: "As an Android developer at the Google Developers Student Club (GDCS), I have had the opportunity to work on exciting projects and collaborate with talented developers who share my passion for technology. Through my involvement with GDCS, I have also had the opportunity to host and participate in numerous events, including hackathons, study jams, and workshops.",
+    //   skills: [
+    //     "Leadership",
+    //     "Mobile Application Development",
+    //     "Kotlin",
+    //     "XML",
+    //     "Figma",
+    //   ],
+    // },
+    // {
+    //   id: 4,
+    //   img: "https://media.licdn.com/dms/image/C4D0BAQH4od9HG2K7bw/company-logo_100_100/0/1661484397513?e=1692230400&v=beta&t=IW-eebE0PpFxqZ8Q3UCYYtlAokbhAEqsNePBv2Pmg18",
+    //   role: "Android Developer Intern",
+    //   company: "Rudraksha Welffare Foundation",
+    //   date: "June 2021 - Oct 2021",
+    //   desc: "As an Android Developer Intern at Rudraksha Welfare Foundation from June 2021 to October 2021, I gained valuable hands-on experience in application development. During my tenure, I contributed to the development of the application's back-end cloud functions using Firebase and implemented a front-end with the MVVM structure, which was designed using Figma. I also added a one-to-one video call feature, conceptualized new features, and optimized app performance. This internship helped me enhance my skills in Android development and gave me an opportunity to work in a collaborative team environment.",
+    //   skills: [
+    //     "Android",
+    //     "Java",
+    //     "Kotlin",
+    //     "XML",
+    //     "Node Js",
+    //     "Cloud Firestore",
+    //     "Firebase",
+    //     "Figma",
+    //   ],
+    //   doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/WhatsApp%20Image%202023-05-05%20at%2012.07.39%20AM.jpeg?alt=media&token=9f0e1648-568b-422d-bd0b-1f125f722245",
+    // },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://en.aahs.ca/ace_media/assets/icon_university_Waterloo.png",
+      school: "University of Waterloo",
+      date: "Sept 2022 - June 2027",
+      grade: "3.9 GPA",
+      desc: "Relevant Courses: Mechanical Engineering Practice I & II [Design and Code], Structure and Properties of Materials, Electrical Engineering for Mechanical Engineers.",
+      degree: "Bachelor of Applied Science - Mechanical Engineering Honours Co-op Program",
+    },
+    {
+      id: 1,
+      img: "https://pickeringhs.ddsb.ca/en/resourcesGeneral/logos/logo.png",
+      school: "Pickering High School",
+      date: "Sept 2018 - June 2022",
+      grade: "97%",
+      desc: "Acquired my Ontario Secondary School Diploma from Pickering High School. Achieved a 97+% average with a Gifted Certificate. Received the OPG Engineering Award for academic excellence in pursuit of an engineering degree. President of  the PHS Engineering Club and active member in PHS Investment Club. Played Junior and Senior Soccer.I completed my class 12 high school education at Methodist School, Dankuni, where I studied Science with Computer Science.",
+      degree: "Ontario Secondary School Diploma",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 9,
+      title: "BrailleBot",
+      date: "May 2023 - Jul 2023",
+      description:
+        "BrailleBot is an EV3 robot with the purpose of translating text files into textured braille, for the visually impaired.",
+      image:
+        "https://i.imgur.com/zatBdA1.jpg",
+      tags: [
+        // "Docker",
+        // "AWS",
+        // "DuckDNS",
+        // "Eslint",
+        // "Husky",
+        // "CI/CD",
+        // "React Js",
+        // "MongoDb",
+        // "Node Js",
+        // "Express Js",
+        // "Redux",
+      ],
+      category: "Design Course",
+      // github: "https://github.com/MSleim/BrailleBot",
+    },
+    {
+      id: 0,
+      title: "SkyPool",
+      date: "Sept 2022 - Dec 2022",
+      description:
+        "SkyPool is a unique product that incorporates enjoyable aspects of numerous games such as Air Hockey, Pool, Pucket, and Darts. Developed for course toy design project.",
+      image:
+        "https://i.imgur.com/wbTMw3h.jpg",
+      // tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
+      category: "Design Course",
+      // member: [
+      //   {
+      //     name: "Rishav Chanda",
+      //     img: "https://avatars.githubusercontent.com/u/64485885?v=4",
+      //     linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+      //     github: "https://github.com/rishavchanda/",
+      //   },
+      //   {
+      //     name: "Upasana Chaudhuri",
+      //     img: "https://avatars.githubusercontent.com/u/100614635?v=4",
+      //     linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+      //     github: "https://github.com/upasana0710",
+      //   },
+      // ],
+    },
+    {
+      id: 1,
+      title: "Driver Modelling System",
+      date: "May 2023 - Present",
+      description:
+        "Detecting drowsiness in drivers, using python libraries OpenCV, dlib, and numpy. Developed to increase road safety and prevent fatigue-related collisions.",
+      image:
+        "https://i.imgur.com/h5WQovg.png",
+      tags: [
+        // "React Js",
+        // "MongoDb",
+        // "Node Js",
+        // "Express Js",
+        // "Redux",
+        // "NodeMailer",
+      ],
+      category: "Professional Initiatives",
+    },
+    {
+      id: 2,
+      title: "3D CAD Design",
+      date: "Sept 2022 - Present",
+      description:
+        "CAD Designs in SolidWorks, NX, and AutoCAD for personal and academic projects.",
+      image:
+        "https://i.imgur.com/c2raWcS.png",
+      // tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
+      category: "Personal Projects",
+      member: [
+        // {
+        //   name: "Rishav Chanda",
+        //   img: "https://avatars.githubusercontent.com/u/64485885?v=4",
+        //   linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+        //   github: "https://github.com/rishavchanda/",
+        // },
+        // {
+        //   name: "Upasana Chaudhuri",
+        //   img: "https://avatars.githubusercontent.com/u/100614635?v=4",
+        //   linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+        //   github: "https://github.com/upasana0710",
+        // },
+      ],
+    },
+  ];
+  
+  export const TimeLineData = [
+    { year: 2017, text: "Started my journey" },
+    { year: 2018, text: "Worked as a freelance developer" },
+    { year: 2019, text: "Founded JavaScript Mastery" },
+    { year: 2020, text: "Shared my projects with the world" },
+    { year: 2021, text: "Started my own platform" },
+  ];
